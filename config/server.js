4866cb60f52b687ef2cc1739ac5a8ffdaf531554
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-03-11 12:33:04 -0600

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a4169321d1f6bacb63fe6e99a6eda97c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d3b813e-128a-4478-a8a9-a32f9fb33b1c'
+};
